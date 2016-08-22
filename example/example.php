790743e334256ef786cd74aef21161d5675055cc
Carlos Herrera <carlos@fusepartners.com.au>
2016-08-22 10:52:55 +1000

    Adding examples

--- example/example.php
+++ example/example.php
@@ -4,10 +4,10 @@ require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Compos
 
 
 $csv = new FullCsv(__DIR__ . '/example.csv');
-$a = "open     ;echo "\n$a\n";var_dump($csv->open());
-$a = "count    ;echo "\n$a\n";var_dump($csv->count());
-$a = "lon      ;echo "\n$a\n";var_dump($csv->longestLine());
-$a = "         ;echo "\n$a\n";var_dump($csv->length);
-$a = "         ;echo "\n$a\n";var_dump($csv->fetchAll());
-$a = "         ;echo "\n$a\n";var_dump($csv->data);
-$a = "         ;echo "\n$a\n";var_dump($csv->close());
+$a = "open     ";echo "\n$a\n";var_dump($csv->open());
+$a = "count    ";echo "\n$a\n";var_dump($csv->count());
+$a = "longest  ";echo "\n$a\n";var_dump($csv->longestLine());
+$a = "lenght   ";echo "\n$a\n";var_dump($csv->length);
+$a = "fetchAll ";echo "\n$a\n";var_dump($csv->fetchAll());
+$a = "data     ";echo "\n$a\n";var_dump($csv->data);
+$a = "close    ";echo "\n$a\n";var_dump($csv->close());
