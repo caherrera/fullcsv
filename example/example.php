f27e79b253897e77b4b39c9a2e5e40bcc9d0f103
Carlos Herrera <caherrerac@falabella.cl>
2019-05-22 21:53:25 -0400

    indenting example file

--- example/example.php
+++ example/example.php
@@ -1,29 +1,47 @@
 <?php
-error_reporting(E_ALL); ini_set('display_errors', 1);
-require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
+require_once dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-$file = (isset($_SERVER['argv'][1])) ? $_SERVER['argv'][1] :__DIR__ . '/example.csv';
+use FullCsv\CsvReader;
 
-try{
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
 
+$file = ( isset( $_SERVER['argv'][1] ) ) ? $_SERVER['argv'][1] : __DIR__ . '/example.csv';
 
-$csv = new FullCsv($file);
-$a = "open     ";echo "\n$a\n";var_dump($csv->open());
-$a = "count    ";echo "\n$a\n";var_dump($csv->count());
-$a = "longest  ";echo "\n$a\n";var_dump($csv->longestLine());
-$a = "length   ";echo "\n$a\n";var_dump($csv->length);
-$a = "fetchAll ";echo "\n$a\n";var_dump($csv->fetchAll());
-$a = "data     ";echo "\n$a\n";var_dump($csv->data);
-$a = "close    ";echo "\n$a\n";var_dump($csv->close());
+try {
 
 
-echo "\n**********************************************";
-echo "\n                    DONE                      ";
-echo "\n**********************************************";
-}catch(Exception $e) {
-    echo "\n**********************************************";
-    echo "\n                    ERRORS                    ";
-    echo "\n**********************************************\n";
+	$csv = new CsvReader( $file );
+	$a   = "open     ";
+	echo "\n$a\n";
+	var_dump( $csv->open() );
+	$a = "count    ";
+	echo "\n$a\n";
+	var_dump( $csv->count() );
+	$a = "longest  ";
+	echo "\n$a\n";
+	var_dump( $csv->longestLine() );
+	$a = "length   ";
+	echo "\n$a\n";
+	var_dump( $csv->length );
+	$a = "fetchAll ";
+	echo "\n$a\n";
+	var_dump( $csv->fetchAll() );
+	$a = "data     ";
+	echo "\n$a\n";
+	var_dump( $csv->data );
+	$a = "close    ";
+	echo "\n$a\n";
+	var_dump( $csv->close() );
 
-    print_r($e);
+
+	echo "\n**********************************************";
+	echo "\n                    DONE                      ";
+	echo "\n**********************************************";
+} catch ( Exception $e ) {
+	echo "\n**********************************************";
+	echo "\n                    ERRORS                    ";
+	echo "\n**********************************************\n";
+
+	print_r( $e );
 }
